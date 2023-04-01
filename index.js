22fbd8397bdc3a9e03b503a5b13f106bdbb030f3
JoshuaFowler <jfowler8912@gmail.com>
2023-03-31 21:27:48 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData( name , email ) {
+  
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(
+        {"name": name, "email": email}
+    ),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object.id);
+    document.body.append(object.id, object.name, object.email)
+  })
+  .catch(function (error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+    document.body.append(error.message)
+  });
+}
